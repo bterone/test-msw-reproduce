c874b86a475edde97bfc3eeee1e0933a2b5dca6c
Terone <bterone@gmail.com>
2023-09-08 17:13:56 +0700

    test: Push MSW test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { setupWorker, rest } from 'msw'
+
+const handlers = [
+  rest.get('http://www.guthib.com', (req, res, ctx) => {
+    return res(
+      ctx.status(201),
+      ctx.json({ username: 'admin' }),
+    )
+  }),
+]
+
+const worker = setupWorker(...handlers)
 
 function App() {
+  worker.start()
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetch('http://www.guthib.com').then(res => res.json()).then(setUser)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +37,13 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
+        {/* This will go through the MSW worker */}
+        {/* {user && <div>{user.username}</div>} */}
+
+        {/* This will not go through the MSW worker and call the real website */}
+        <iframe src="http://www.guthib.com" title="test"></iframe>
       </header>
     </div>
   );
